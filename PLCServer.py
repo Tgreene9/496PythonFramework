3446fd654915b6180eb44d811d552119ad389dcb
Tgreene9 <91227431+Tgreene9@users.noreply.github.com>
2023-06-08 04:28:09 -0700

    Update PLCServer.py

--- PLCServer.py
+++ PLCServer.py
@@ -1,9 +1,14 @@
 # Import required libraries
-from pymodbus.server import StartTcpServer
+import random
+import logging
 from pymodbus.device import ModbusDeviceIdentification
-from pymodbus.datastore import ModbusSequentialDataBlock
-from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-from random import randrange, sample
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSlaveContext, ModbusServerContext
+from pymodbus.server.async_io import StartTcpServer
+
+# Enable logging (makes it easier to debug if something goes wrong)
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 # Define your device identification
 device_identification = ModbusDeviceIdentification()
@@ -14,30 +19,29 @@ device_identification.ProductName = 'pymodbus Server'
 device_identification.ModelName   = 'pymodbus Server'
 device_identification.MajorMinorRevision = '1.0'
 
-# Create a function to generate a list with 10000 addresses and 10 random values
-def create_data_store(value_range):
-    data_store = [0 for _ in range(10000)]
-    for i in sample(range(10000), 10):
-        data_store[i] = randrange(value_range)
-    return data_store
+# Define the Modbus registers
+coils = ModbusSequentialDataBlock(0, [False] * 5)
+discrete_inputs = ModbusSequentialDataBlock(1, [False] * 6)
+holding_registers = ModbusSequentialDataBlock(1, [1234])
+input_registers = ModbusSequentialDataBlock(1, [8888])
 
-# Create data stores and populate them with random values
-coil_store = ModbusSequentialDataBlock(0, create_data_store(2))
-di_store = ModbusSequentialDataBlock(0, create_data_store(2))
-hr_store = ModbusSequentialDataBlock(0, create_data_store(65535))
-ir_store = ModbusSequentialDataBlock(0, create_data_store(65535))
+temperature_values = [random.randint(4, 15) for _ in range(7)]
+holding_registers.setValues(1, temperature_values)
+print("temperature_values:", temperature_values)
 
-# Create slave context
+
+# Define the Modbus slave context
 slave_context = ModbusSlaveContext(
-    di=di_store,
-    co=coil_store,
-    hr=hr_store,
-    ir=ir_store,
-    zero_mode=True
+    di=discrete_inputs,
+    co=coils,
+    hr=holding_registers,
+    ir=input_registers
 )
 
-# Create server context
+# Define the Modbus server context
 server_context = ModbusServerContext(slaves=slave_context, single=True)
 
-# Start the server
-StartTcpServer(context=server_context, identity=device_identification)
+# Start the Modbus TCP server
+StartTcpServer(context=server_context, address=("localhost", 502))
+
+
